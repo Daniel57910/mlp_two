1e1d5474e98ae58cafde6339e6214470160fc41a
Daniel Miller <danielmiller@Daniels-MacBook-Pro.local>
2022-11-27 13:38:28 +0000

    bnorm finished with apool block

--- pytorch_mlp_framework/model_architectures_dm.py
+++ pytorch_mlp_framework/model_architectures_dm.py
@@ -51,3 +51,58 @@ class ConvolutionalProcessingBlockDM(nn.Module):
         x = self.block_one(x)
         x = self.block_two(x)
         return x
+    
+
+class ConvolutionalReductionBlockDM(nn.Module):
+    def __init__(self, input_shape, output_filters, kernel_size, padding, bias, dilation, reduction_factor):
+        super(ConvolutionalReductionBlockDM, self).__init__()
+
+        """
+        Iitialize the convolutional processing block
+            :param input_shape: tuple of ints, shape of input tensor (batch_size, channels, height, width)
+            :param output_filters: int, number of output filters
+            :param kernel_size: int, size of convolutional kernel
+            :param padding: int, padding of convolutional kernel
+            :param bias: bool, whether to use bias in convolutional layers
+            :param dilation: int, dilation of convolutional kernel
+            :param reduction_factor: int, factor by which to reduce the height and width of the input tensor
+        """
+        self.input_filters = input_shape[1]
+        self.output_filters = output_filters
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.bias = bias
+        self.dilation = dilation
+    
+        self.block_one = nn.Sequential(
+            nn.Conv2d(
+                self.input_filters, 
+                self.output_filters, 
+                self.kernel_size, 
+                padding=self.padding, 
+                bias=self.bias,
+                dilation=self.dilation),
+            nn.BatchNorm2d(self.output_filters),
+            nn.LeakyReLU(),
+        )
+
+        self.apool = nn.AvgPool2d(reduction_factor)
+
+        self.block_two = nn.Sequential(
+            nn.Conv2d(
+                self.output_filters,
+                self.output_filters,
+                self.kernel_size,
+                padding=self.padding,
+                bias=self.bias,
+                dilation=self.dilation
+            ), 
+            nn.BatchNorm2d(self.output_filters),
+            nn.LeakyReLU(),
+        )  
+            
+    def forward(self, x):
+        x = self.block_one(x)
+        x = self.apool(x)
+        x = self.block_two(x)
+        return x
